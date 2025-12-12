ba5b3b1557d68a78a56cef1030368e273a83977c
Chris <140977972+Cbuen@users.noreply.github.com>
2025-12-11 22:26:06 -0800

    Create basic homeworkview

--- myDayAPI/api/serializers.py
+++ myDayAPI/api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import TodoList, Todo
+from .models import TodoList, Todo, Homework
+
 
 class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoList
-        fields = '__all__'
+        fields = "__all__"
+
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = "__all__"
+
+
+class HomeworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Homework
+        fields = "__all__"
